507fe581b9fdbb2f2d35aff7838c8685787ce92b
adi4fab <fabregas4aditya@gmail.com>
2021-06-14 13:37:13 +0530

    all

---
+++ providers.tf
@@ -0,0 +1,38 @@
+##########################################
+######## Configure Terraform Block #######
+##########################################
+
+
+terraform {
+  required_version = "~>0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+
+##########################################
+####### Configure AWS Provider ###########
+##########################################
+
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "terraform_user"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "statefilebucketforukterrform"
+  acl    = "public-read-write"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "statefilebucketforukterrform"
+    key    = "statefile"
+    region = "us-east-1"
+  }
+}
